a4db3fb3c20a4fd104a505d10df846ad1f88d000
hemoreno33 <hemoreno33@misena.edu.co>
2015-03-10 16:44:50 -0500

    Ejemplos de Log4J
    ejemplos de log4j

---
+++ java/EjemploLog4J/src/main/java/edu/co/sena/ejemplolog4j/ejemplo1/ejemplolog4j/Ejemplo1Log4J.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.co.sena.ejemplolog4j.ejemplo1.ejemplolog4j;
+
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+/**
+ *
+ * @author Enrique Moreno
+ */
+public class Ejemplo1Log4J {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         Logger log = Logger.getLogger("Ejemplo1Log4J.class");
+         
+         PropertyConfigurator.configure("log4jDB.properties");
+         
+         log.fatal("esto es un error fatal");
+         log.error("esto es un error");
+         log.warn("esto es una alvertencia"); 
+         log.info("esto es un mensaje de información");
+         log.debug("esto es un mensaje de debug");
+         log.trace("esto es un mensaje de razabilidad");
+         
+         
+    }
+    
+}
